bc3cd8b95e91d63931fc58f58893d0f817d10e89
Matthew Garrett <mjg59@google.com>
2018-12-12 13:34:38 -0800

    tpm: Append the final event log to the TPM event log
    Any events that are logged after GetEventsLog() is called are logged to
    the EFI Final Events table. These events are defined as being in the
    crypto agile log format, so we can just append them directly to the
    existing log if it's in the same format. In theory we can also construct
    old-style SHA1 log entries for devices that only return logs in that
    format, but EDK2 doesn't generate the final event log in that case so
    it doesn't seem worth it at the moment.
    
    Signed-off-by: Matthew Garrett <mjg59@google.com>

--- drivers/char/tpm/eventlog/efi.c
+++ drivers/char/tpm/eventlog/efi.c
@@ -21,6 +21,7 @@
 int tpm_read_log_efi(struct tpm_chip *chip)
 {
 
+	struct efi_tcg2_final_events_table *final_tbl = NULL;
 	struct linux_efi_tpm_eventlog *log_tbl;
 	struct tpm_bios_log *log;
 	u32 log_size;
@@ -55,12 +56,34 @@ int tpm_read_log_efi(struct tpm_chip *chip)
 	if (!log->bios_event_log)
 		goto err_memunmap;
 	log->bios_event_log_end = log->bios_event_log + log_size;
-
 	tpm_log_version = log_tbl->version;
+
+	if (efi.tpm_final_log != EFI_INVALID_TABLE_ADDR) {
+		if (tpm_log_version == EFI_TCG2_EVENT_LOG_FORMAT_TCG_2) {
+			final_tbl = memremap(efi.tpm_final_log,
+				   sizeof(*final_tbl) + efi_tpm_final_log_size,
+				   MEMREMAP_WB);
+			if (!final_tbl) {
+				pr_err("Could not map UEFI TPM final log\n");
+				goto err_memunmap;
+			}
+
+			log->bios_event_log = krealloc(log->bios_event_log,
+					     log_size + efi_tpm_final_log_size,
+					     GFP_KERNEL);
+			if (!log->bios_event_log)
+				goto err_memunmap;
+			memcpy((void *)log->bios_event_log + log_size,
+			       final_tbl->events, efi_tpm_final_log_size);
+			log->bios_event_log_end += efi_tpm_final_log_size;
+		}
+	}
+	memunmap(final_tbl);
 	memunmap(log_tbl);
 	return tpm_log_version;
 
 err_memunmap:
+	memunmap(final_tbl);
 	memunmap(log_tbl);
 	return -ENOMEM;
 }
